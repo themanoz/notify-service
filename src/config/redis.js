4ea81c40cb84f2dd0d86e95723bd2af7dbd11744
Manoj <manojkumarbonala3@gmail.com>
2025-07-05 11:08:37 +0530

    updated redis file

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,9 +1,7 @@
 const Redis = require("ioredis");
 
 const redis = new Redis({
-  host: process.env.REDIS_HOST || "127.0.0.1",
-  port: Number(process.env.REDIS_PORT || 6379),
-  maxRetriesPerRequest: null,
+  host: process.env.REDIS_URL,
 });
 
 module.exports = redis;
