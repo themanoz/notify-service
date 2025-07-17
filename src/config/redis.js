6df00cc1e31e2173beefe2c01e4f98ae3eeedb63
Manoj <manojkumarbonala3@gmail.com>
2025-07-17 11:50:54 +0530

    update code

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,8 +1,10 @@
 const Redis = require("ioredis");
 
 const redis = new Redis(process.env.UPSTASH_REDIS_URL, {
-    maxRetriesPerRequest: null,  
-    enableReadyCheck: false      
-  });
+  maxRetriesPerRequest: null,
+  enableReadyCheck: false,
+  // family: 6,
+});
+redis.ping().then(console.log).catch(console.error);
 
 module.exports = redis;
