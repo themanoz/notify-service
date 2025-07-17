7e5485d2b67a7ff2a1bdbd0df2f414bba9a4e2c9
Manoj <manojkumarbonala3@gmail.com>
2025-07-17 12:19:43 +0530

    updated redis config

--- src/config/redis.js
+++ src/config/redis.js
@@ -6,7 +6,7 @@ dotenv.config();
 const redis = new Redis(process.env.UPSTASH_REDIS_URL, {
   maxRetriesPerRequest: null,
   enableReadyCheck: false,
-  // family: 6,
+  
 });
 redis.ping().then(console.log).catch(console.error);
 
